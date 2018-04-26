84260612608d55e8e81298662c29116abe696ff0
Eric Sullivan <eric.sullivan@annkissam.com>
2018-04-26 10:15:47 -0400

    fix for missing plug depenedency

--- lib/aegis/errors.ex
+++ lib/aegis/errors.ex
@@ -1,11 +1,13 @@
-defmodule Aegis.AuthorizationNotPerformedError do
-  @moduledoc """
-  Raised when authorization has not been implemented by the end of the Plug
-  pipeline.
-  """
-  defexception [conn: nil, message: "Not Authorized"]
-end
+if Code.ensure_loaded?(Phoenix) do
+  defmodule Aegis.AuthorizationNotPerformedError do
+    @moduledoc """
+    Raised when authorization has not been implemented by the end of the Plug
+    pipeline.
+    """
+    defexception [conn: nil, message: "Not Authorized"]
+  end
 
-defimpl Plug.Exception, for: Aegis.AuthorizationNotPerformedError do
-  def status(_exception), do: 403
+  defimpl Plug.Exception, for: Aegis.AuthorizationNotPerformedError do
+    def status(_exception), do: 403
+  end
 end
