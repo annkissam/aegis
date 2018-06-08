0156dc538e581d4b2a1b4fd6b5ba10c7d1b5ab7f
Joshua Adams <josh.adams@annkissam.com>
2018-06-08 08:12:01 -0400

    moves phoenix-related errors into `AegisController`

--- lib/aegis_phoenix/controller.ex
+++ lib/aegis_phoenix/controller.ex
@@ -4,6 +4,18 @@ if Code.ensure_loaded?(Phoenix) do
     Wraps controllers with Aegis authorization functionality.
     """
 
+    defmodule Aegis.AuthorizationNotPerformedError do
+      @moduledoc """
+      Raised when authorization has not been implemented by the end of the Plug
+      pipeline.
+      """
+      defexception [conn: nil, message: "Not Authorized"]
+    end
+
+    defimpl Plug.Exception, for: Aegis.AuthorizationNotPerformedError do
+      def status(_exception), do: 403
+    end
+
     @doc """
     Authorizes a resource, for a user, for a given action, and marks the
     connection as having had aegis authorization perfomed via the assignment of a
